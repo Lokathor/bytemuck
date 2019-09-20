d4fde533861667477eb8d46ea795303ca83496dc
Lokathor <zefria@gmail.com>
2019-09-20 11:10:01 -0600

    Update src/zeroable.rs
    Co-Authored-By: Daniel Henry-Mantilla <daniel.henry.mantilla@gmail.com>

--- src/zeroable.rs
+++ src/zeroable.rs
@@ -14,6 +14,7 @@ pub unsafe trait Zeroable: Sized {
   /// This is a trait method so that you can write `MyType::zeroed()` in your
   /// code. It is a contract of this trait that if you implement it on your type
   /// you **must not** override this method.
+  #[inline]
   fn zeroed() -> Self {
     unsafe { core::mem::zeroed() }
   }
