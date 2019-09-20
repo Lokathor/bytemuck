c7e124e641c41beaf2d81bbed9af90e4ac60bdd6
Lokathor <zefria@gmail.com>
2019-09-20 00:52:55 -0600

    Remove Arc and Rc support, they've got repr(rust) internals.

--- src/allocation.rs
+++ src/allocation.rs
@@ -3,34 +3,6 @@
 use super::*;
 use alloc::{boxed::Box, rc::Rc, sync::Arc, vec::Vec};
 
-/// As [`try_cast_arc`](try_cast_arc), but unwraps for you.
-#[inline]
-pub fn cast_arc<A: Pod, B: Pod>(input: Arc<A>) -> Arc<B> {
-  try_cast_arc(input).map_err(|(e, _v)| e).unwrap()
-}
-
-/// Attempts to cast the content type of a [`Arc`](alloc::sync::Arc).
-///
-/// On failure you get back an error along with the starting `Arc`.
-///
-/// ## Failure
-///
-/// * The start and end content type of the `Arc` must have the exact same
-///   alignment.
-/// * The start and end size of the `Arc` must have the exact same size.
-#[inline]
-pub fn try_cast_arc<A: Pod, B: Pod>(input: Arc<A>) -> Result<Arc<B>, (PodCastError, Arc<A>)> {
-  if align_of::<A>() != align_of::<B>() {
-    Err((PodCastError::AlignmentMismatch, input))
-  } else if size_of::<A>() != size_of::<B>() {
-    Err((PodCastError::SizeMismatch, input))
-  } else {
-    // Note(Lokathor): This is much simpler than with the Vec casting!
-    let ptr: *mut B = Arc::into_raw(input) as *mut B;
-    Ok(unsafe { Arc::from_raw(ptr) })
-  }
-}
-
 /// As [`try_cast_box`](try_cast_box), but unwraps for you.
 #[inline]
 pub fn cast_box<A: Pod, B: Pod>(input: Box<A>) -> Box<B> {
@@ -56,34 +28,6 @@ pub fn try_cast_box<A: Pod, B: Pod>(input: Box<A>) -> Result<Box<B>, (PodCastErr
     // Note(Lokathor): This is much simpler than with the Vec casting!
     let ptr: *mut B = Box::into_raw(input) as *mut B;
     Ok(unsafe { Box::from_raw(ptr) })
-  }
-}
-
-/// As [`try_cast_rc`](try_cast_rc), but unwraps for you.
-#[inline]
-pub fn cast_rc<A: Pod, B: Pod>(input: Rc<A>) -> Rc<B> {
-  try_cast_rc(input).map_err(|(e, _v)| e).unwrap()
-}
-
-/// Attempts to cast the content type of a [`Rc`](alloc::rc::Rc).
-///
-/// On failure you get back an error along with the starting `Rc`.
-///
-/// ## Failure
-///
-/// * The start and end content type of the `Rc` must have the exact same
-///   alignment.
-/// * The start and end size of the `Rc` must have the exact same size.
-#[inline]
-pub fn try_cast_rc<A: Pod, B: Pod>(input: Rc<A>) -> Result<Rc<B>, (PodCastError, Rc<A>)> {
-  if align_of::<A>() != align_of::<B>() {
-    Err((PodCastError::AlignmentMismatch, input))
-  } else if size_of::<A>() != size_of::<B>() {
-    Err((PodCastError::SizeMismatch, input))
-  } else {
-    // Note(Lokathor): This is much simpler than with the Vec casting!
-    let ptr: *mut B = Rc::into_raw(input) as *mut B;
-    Ok(unsafe { Rc::from_raw(ptr) })
   }
 }
 
