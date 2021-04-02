f3e16c7051b561b027c797823b3ca16e7133028d
Zakarum <zakarumych@ya.ru>
2021-04-02 04:56:49 +0300

    Implement Pod and Zeroable for arrays of any size (#59)
    * Implement Pod and Zeroable for arrays of any size
    
    Only when rustc version is 1.51 or newer
    
    * Rename cfg flag after feature name

--- src/pod.rs
+++ src/pod.rs
@@ -66,6 +66,10 @@ unsafe impl<T: Pod> Pod for ManuallyDrop<T> {}
 
 // Note(Lokathor): MaybeUninit can NEVER be Pod.
 
+#[cfg(min_const_generics)]
+unsafe impl<T, const N: usize> Pod for [T; N] where T: Pod {}
+
+#[cfg(not(min_const_generics))]
 impl_unsafe_marker_for_array!(
   Pod, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
   20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 48, 64, 96, 128, 256,
