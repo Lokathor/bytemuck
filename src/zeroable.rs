223455499aabdeae341d5e004c53fc31a9317861
Connor <chorman64@gmail.com>
2021-06-04 11:19:02 -0400

    Attach min_const_generics to a crate feature

--- src/zeroable.rs
+++ src/zeroable.rs
@@ -109,10 +109,10 @@ unsafe impl<
 {
 }
 
-#[cfg(min_const_generics)]
+#[cfg(any(min_const_generics, feature = "min_const_generics"))]
 unsafe impl<T, const N: usize> Zeroable for [T; N] where T: Zeroable {}
 
-#[cfg(not(min_const_generics))]
+#[cfg(not(any(min_const_generics, feature = "min_const_generics")))]
 impl_unsafe_marker_for_array!(
   Zeroable, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18,
   19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 48, 64, 96, 128, 256,
