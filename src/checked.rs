Lokathor/bytemuck

commit b472189ff8cc51c0ee01e4d121b86fbbc706d286

Add `NoPadding`, `AnyBitPattern`, and `CheckedBitPattern` traits (#91)
* add MaybePod and NoPadding traits

* MaybePod and NoPadding derive macros

* fix doctest

* fmt

* fix bad doc link

* move new casting functions into separate modules

* fmt

* fix doctest and derive test

* remove relaxed module, add anybitpattern

* rename MaybePod to CheckedCastFromPod

* rename checked casting functions

* rework CheckedCastFromPod into CheckedBitPattern

* add anybitpattern derive, fix up other derives

* fix doctest

* fix derive trait of bits type

* export AnyBitPattern derive

* export anybitpattern from traits

* actually export derive macro for AnyBitPattern

* make bits struct pub because of type leaking rules

* allow clippy lint in derive

* add copy bound to CheckedBitPattern

* - replace Pod bounds with NoPadding+AnyBitPattern
- add try and panic versions of checked cast functions
- slightly update docs

* fix derive tests

* - adapt the allocation module cast functions as well
- as part of that, make AnyBitPattern a subtrait of Zeroable
- AnyBitPattern derive also derives Zeroable

* @JakobDegen and @zakarumych nits

* superset -> subset on CheckedBitPattern and NoPadding docs

* derive Debug on generated `Bits` structs, which can be useful for debugging failures

* don't derive debug on spirv target arch

* make it work on 1.34

* merge conflicts

* fix erroneous behavior in doctest

src/checked.rs
@@ -1,0 +1,456 @@
+//! Checked versions of the casting functions exposed in crate root
+//! that support [`CheckedBitPattern`] types.
+
+use crate::{internal::{self, something_went_wrong}, NoPadding, AnyBitPattern};
+
+/// A marker trait that allows types that have some invalid bit patterns to be used
+/// in places that otherwise require [`AnyBitPattern`] or [`Pod`] types by performing
+/// a runtime check on a perticular set of bits. This is particularly
+/// useful for types like fieldless ('C-style') enums, [`char`], bool, and structs containing them.
+///
+/// To do this, we define a `Bits` type which is a type with equivalent layout
+/// to `Self` other than the invalid bit patterns which disallow `Self` from
+/// being [`AnyBitPattern`]. This `Bits` type must itself implement [`AnyBitPattern`].
+/// Then, we implement a function that checks wheter a certain instance
+/// of the `Bits` is also a valid bit pattern of `Self`. If this check passes, then we
+/// can allow casting from the `Bits` to `Self` (and therefore, any type which
+/// is able to be cast to `Bits` is also able to be cast to `Self`).
+///
+/// [`AnyBitPattern`] is a subset of [`CheckedBitPattern`], meaning that any `T: AnyBitPattern` is also
+/// [`CheckedBitPattern`]. This means you can also use any [`AnyBitPattern`] type in the checked versions
+/// of casting functions in this module. If it's possible, prefer implementing [`AnyBitPattern`] for your
+/// type directly instead of [`CheckedBitPattern`] as it gives greater flexibility.
+///
+/// # Derive
+///
+/// A `#[derive(CheckedBitPattern)]` macro is provided under the `derive` feature flag which will
+/// automatically validate the requirements of this trait and implement the
+/// trait for you for both enums and structs. This is the recommended method for
+/// implementing the trait, however it's also possible to do manually.
+///
+/// # Example
+///
+/// If manually implementing the trait, we can do something like so:
+///
+/// ```rust
+/// use bytemuck::{CheckedBitPattern, NoPadding};
+///
+/// #[repr(u32)]
+/// #[derive(Copy, Clone)]
+/// enum MyEnum {
+///     Variant0 = 0,
+///     Variant1 = 1,
+///     Variant2 = 2,
+/// }
+///
+/// unsafe impl CheckedBitPattern for MyEnum {
+///     type Bits = u32;
+///
+///     fn is_valid_bit_pattern(bits: &u32) -> bool {
+///         match *bits {
+///             0 | 1 | 2 => true,
+///             _ => false,
+///         }
+///     }
+/// }
+/// 
+/// // It is often useful to also implement `NoPadding` on our `CheckedBitPattern` types.
+/// // This will allow us to do casting of mutable references (and mutable slices).
+/// // It is not always possible to do so, but in this case we have no padding so it is.
+/// unsafe impl NoPadding for MyEnum {}
+/// ```
+///
+/// We can now use relevant casting functions. For example,
+///
+/// ```rust
+/// # use bytemuck::{CheckedBitPattern, NoPadding};
+/// # #[repr(u32)]
+/// # #[derive(Copy, Clone, PartialEq, Eq, Debug)]
+/// # enum MyEnum {
+/// #     Variant0 = 0,
+/// #     Variant1 = 1,
+/// #     Variant2 = 2,
+/// # }
+/// # unsafe impl NoPadding for MyEnum {}
+/// # unsafe impl CheckedBitPattern for MyEnum {
+/// #     type Bits = u32;
+/// #     fn is_valid_bit_pattern(bits: &u32) -> bool {
+/// #         match *bits {
+/// #             0 | 1 | 2 => true,
+/// #             _ => false,
+/// #         }
+/// #     }
+/// # }
+/// use bytemuck::{bytes_of, bytes_of_mut};
+/// use bytemuck::checked;
+///
+/// let bytes = bytes_of(&2u32);
+/// let result = checked::try_from_bytes::<MyEnum>(bytes);
+/// assert_eq!(result, Ok(&MyEnum::Variant2));
+///
+/// // Fails for invalid discriminant
+/// let bytes = bytes_of(&100u32);
+/// let result = checked::try_from_bytes::<MyEnum>(bytes);
+/// assert!(result.is_err());
+/// 
+/// // Since we implemented NoPadding, we can also cast mutably from an original type
+/// // that is `NoPadding + AnyBitPattern`:
+/// let mut my_u32 = 2u32;
+/// {
+///   let as_enum_mut = checked::cast_mut::<_, MyEnum>(&mut my_u32);
+///   assert_eq!(as_enum_mut, &mut MyEnum::Variant2);
+///   *as_enum_mut = MyEnum::Variant0;
+/// }
+/// assert_eq!(my_u32, 0u32);
+/// ```
+///
+/// # Safety
+///
+/// * `Self` *must* have the same layout as the specified `Bits` except for
+/// the possible invalid bit patterns being checked during [`is_valid_bit_pattern`].
+///   * This almost certainly means your type must be `#[repr(C)]` or a similar
+///   specified repr, but if you think you know better, you probably don't. If you
+///   still think you know better, be careful and have fun. And don't mess it up
+///   (I mean it).
+/// * If [`is_valid_bit_pattern`] returns true, then the bit pattern contained in
+///   `bits` must also be valid for an instance of `Self`.
+/// * Probably more, don't mess it up (I mean it 2.0)
+///
+/// [`is_valid_bit_pattern`]: CheckedBitPattern::is_valid_bit_pattern
+/// [`Pod`]: crate::Pod
+pub unsafe trait CheckedBitPattern: Copy {
+  /// `Self` *must* have the same layout as the specified `Bits` except for
+  /// the possible invalid bit patterns being checked during [`is_valid_bit_pattern`].
+  ///
+  /// [`is_valid_bit_pattern`]: CheckedBitPattern::is_valid_bit_pattern
+  type Bits: AnyBitPattern;
+
+  /// If this function returns true, then it must be valid to reinterpret `bits` as `&Self`.
+  fn is_valid_bit_pattern(bits: &Self::Bits) -> bool;
+}
+
+unsafe impl<T: AnyBitPattern> CheckedBitPattern for T {
+  type Bits = T;
+
+  #[inline(always)]
+  fn is_valid_bit_pattern(_bits: &T) -> bool {
+    true
+  }
+}
+
+unsafe impl CheckedBitPattern for char {
+  type Bits = u32;
+
+  #[inline]
+  fn is_valid_bit_pattern(bits: &Self::Bits) -> bool {
+    core::char::from_u32(*bits).is_some()
+  }
+}
+
+unsafe impl CheckedBitPattern for bool {
+  type Bits = u8;
+
+  #[inline]
+  fn is_valid_bit_pattern(bits: &Self::Bits) -> bool {
+    match *bits {
+      0 | 1 => true,
+      _ => false,
+    }
+  }
+}
+
+/// The things that can go wrong when casting between [`CheckedBitPattern`] data forms.
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub enum CheckedCastError {
+  /// An error occurred during a true-[`Pod`] cast
+  PodCastError(crate::PodCastError),
+  /// When casting to a [`CheckedBitPattern`] type, it is possible that the original
+  /// data contains an invalid bit pattern. If so, the cast will fail and
+  /// this error will be returned. Will never happen on casts between
+  /// [`Pod`] types.
+  InvalidBitPattern,
+}
+
+#[cfg(not(target_arch = "spirv"))]
+impl core::fmt::Display for CheckedCastError {
+  fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
+    write!(f, "{:?}", self)
+  }
+}
+#[cfg(feature = "extern_crate_std")]
+impl std::error::Error for CheckedCastError {}
+
+impl From<crate::PodCastError> for CheckedCastError {
+  fn from(err: crate::PodCastError) -> CheckedCastError {
+    CheckedCastError::PodCastError(err)
+  }
+}
+
+/// Re-interprets `&[u8]` as `&T`.
+///
+/// ## Failure
+///
+/// * If the slice isn't aligned for the new type
+/// * If the slice's length isn’t exactly the size of the new type
+/// * If the slice contains an invalid bit pattern for `T`
+#[inline]
+pub fn try_from_bytes<T: CheckedBitPattern>(
+  s: &[u8],
+) -> Result<&T, CheckedCastError> {
+  let pod = unsafe { internal::try_from_bytes(s) }?;
+
+  if <T as CheckedBitPattern>::is_valid_bit_pattern(pod) {
+    Ok(unsafe { &*(pod as *const <T as CheckedBitPattern>::Bits as *const T) })
+  } else {
+    Err(CheckedCastError::InvalidBitPattern)
+  }
+}
+
+/// Re-interprets `&mut [u8]` as `&mut T`.
+///
+/// ## Failure
+///
+/// * If the slice isn't aligned for the new type
+/// * If the slice's length isn’t exactly the size of the new type
+/// * If the slice contains an invalid bit pattern for `T`
+#[inline]
+pub fn try_from_bytes_mut<T: CheckedBitPattern + NoPadding>(
+  s: &mut [u8],
+) -> Result<&mut T, CheckedCastError> {
+  let pod = unsafe { internal::try_from_bytes_mut(s) }?;
+
+  if <T as CheckedBitPattern>::is_valid_bit_pattern(pod) {
+    Ok(unsafe { &mut *(pod as *mut <T as CheckedBitPattern>::Bits as *mut T) })
+  } else {
+    Err(CheckedCastError::InvalidBitPattern)
+  }
+}
+
+/// Reads from the bytes as if they were a `T`.
+///
+/// ## Failure
+/// * If the `bytes` length is not equal to `size_of::<T>()`.
+/// * If the slice contains an invalid bit pattern for `T`
+#[inline]
+pub fn try_pod_read_unaligned<T: CheckedBitPattern>(bytes: &[u8]) -> Result<T, CheckedCastError> {
+  let pod = unsafe { internal::try_pod_read_unaligned(bytes) }?;
+
+  if <T as CheckedBitPattern>::is_valid_bit_pattern(pod) {
+    Ok(unsafe { transmute!(pod) })
+  } else {
+    Err(CheckedCastError::InvalidBitPattern)
+  }
+}
+
+/// Try to cast `T` into `U`.
+///
+/// Note that for this particular type of cast, alignment isn't a factor. The
+/// input value is semantically copied into the function and then returned to a
+/// new memory location which will have whatever the required alignment of the
+/// output type is.
+///
+/// ## Failure
+///
+/// * If the types don't have the same size this fails.
+/// * If `a` contains an invalid bit pattern for `B` this fails.
+#[inline]
+pub fn try_cast<A: NoPadding, B: CheckedBitPattern>(
+  a: A,
+) -> Result<B, CheckedCastError> {
+  let pod = unsafe { internal::try_cast(a) }?;
+
+  if <B as CheckedBitPattern>::is_valid_bit_pattern(&pod) {
+    Ok(unsafe { transmute!(pod) })
+  } else {
+    Err(CheckedCastError::InvalidBitPattern)
+  }
+}
+
+/// Try to convert a `&T` into `&U`.
+///
+/// ## Failure
+///
+/// * If the reference isn't aligned in the new type
+/// * If the source type and target type aren't the same size.
+/// * If `a` contains an invalid bit pattern for `B` this fails.
+#[inline]
+pub fn try_cast_ref<A: NoPadding, B: CheckedBitPattern>(
+  a: &A,
+) -> Result<&B, CheckedCastError> {
+  let pod = unsafe { internal::try_cast_ref(a) }?;
+
+  if <B as CheckedBitPattern>::is_valid_bit_pattern(pod) {
+    Ok(unsafe { &*(pod as *const <B as CheckedBitPattern>::Bits as *const B) })
+  } else {
+    Err(CheckedCastError::InvalidBitPattern)
+  }
+}
+
+/// Try to convert a `&mut T` into `&mut U`.
+///
+/// As [`checked_cast_ref`], but `mut`.
+#[inline]
+pub fn try_cast_mut<A: NoPadding + AnyBitPattern, B: CheckedBitPattern + NoPadding>(
+  a: &mut A,
+) -> Result<&mut B, CheckedCastError> {
+  let pod = unsafe { internal::try_cast_mut(a) }?;
+
+  if <B as CheckedBitPattern>::is_valid_bit_pattern(pod) {
+    Ok(unsafe { &mut *(pod as *mut <B as CheckedBitPattern>::Bits as *mut B) })
+  } else {
+    Err(CheckedCastError::InvalidBitPattern)
+  }
+}
+
+/// Try to convert `&[A]` into `&[B]` (possibly with a change in length).
+///
+/// * `input.as_ptr() as usize == output.as_ptr() as usize`
+/// * `input.len() * size_of::<A>() == output.len() * size_of::<B>()`
+///
+/// ## Failure
+///
+/// * If the target type has a greater alignment requirement and the input slice
+///   isn't aligned.
+/// * If the target element type is a different size from the current element
+///   type, and the output slice wouldn't be a whole number of elements when
+///   accounting for the size change (eg: 3 `u16` values is 1.5 `u32` values, so
+///   that's a failure).
+/// * Similarly, you can't convert between a [ZST](https://doc.rust-lang.org/nomicon/exotic-sizes.html#zero-sized-types-zsts)
+///   and a non-ZST.
+/// * If any element of the converted slice would contain an invalid bit pattern for `B` this fails.
+#[inline]
+pub fn try_cast_slice<A: NoPadding, B: CheckedBitPattern>(
+  a: &[A],
+) -> Result<&[B], CheckedCastError> {
+  let pod = unsafe { internal::try_cast_slice(a) }?;
+
+  if pod.iter().all(|pod| <B as CheckedBitPattern>::is_valid_bit_pattern(pod)) {
+    Ok(unsafe {
+      core::slice::from_raw_parts(pod.as_ptr() as *const B, pod.len())
+    })
+  } else {
+    Err(CheckedCastError::InvalidBitPattern)
+  }
+}
+
+/// Try to convert `&mut [A]` into `&mut [B]` (possibly with a change in
+/// length).
+///
+/// As [`checked_cast_slice`], but `&mut`.
+#[inline]
+pub fn try_cast_slice_mut<A: NoPadding + AnyBitPattern, B: CheckedBitPattern + NoPadding>(
+  a: &mut [A],
+) -> Result<&mut [B], CheckedCastError> {
+  let pod = unsafe { internal::try_cast_slice_mut(a) }?;
+
+  if pod.iter().all(|pod| <B as CheckedBitPattern>::is_valid_bit_pattern(pod)) {
+    Ok(unsafe {
+      core::slice::from_raw_parts_mut(pod.as_ptr() as *mut B, pod.len())
+    })
+  } else {
+    Err(CheckedCastError::InvalidBitPattern)
+  }
+}
+
+/// Re-interprets `&[u8]` as `&T`.
+///
+/// ## Panics
+///
+/// This is [`try_from_bytes`] but will panic on error.
+#[inline]
+pub fn from_bytes<T: CheckedBitPattern>(s: &[u8]) -> &T {
+  match try_from_bytes(s) {
+    Ok(t) => t,
+    Err(e) => something_went_wrong("from_bytes", e),
+  }
+}
+
+/// Re-interprets `&mut [u8]` as `&mut T`.
+///
+/// ## Panics
+///
+/// This is [`try_from_bytes_mut`] but will panic on error.
+#[inline]
+pub fn from_bytes_mut<T: NoPadding + CheckedBitPattern>(s: &mut [u8]) -> &mut T {
+  match try_from_bytes_mut(s) {
+    Ok(t) => t,
+    Err(e) => something_went_wrong("from_bytes_mut", e),
+  }
+}
+
+/// Reads the slice into a `T` value.
+///
+/// ## Panics
+/// * This is like `try_pod_read_unaligned` but will panic on failure.
+#[inline]
+pub fn pod_read_unaligned<T: AnyBitPattern>(bytes: &[u8]) -> T {
+  match try_pod_read_unaligned(bytes) {
+    Ok(t) => t,
+    Err(e) => something_went_wrong("pod_read_unaligned", e),
+  }
+}
+
+/// Cast `T` into `U`
+///
+/// ## Panics
+///
+/// * This is like [`try_cast`](try_cast), but will panic on a size mismatch.
+#[inline]
+pub fn cast<A: NoPadding, B: CheckedBitPattern>(a: A) -> B {
+  match try_cast(a) {
+    Ok(t) => t,
+    Err(e) => something_went_wrong("cast", e),
+  }
+}
+
+/// Cast `&mut T` into `&mut U`.
+///
+/// ## Panics
+///
+/// This is [`try_cast_mut`] but will panic on error.
+#[inline]
+pub fn cast_mut<A: NoPadding + AnyBitPattern, B: NoPadding + CheckedBitPattern>(a: &mut A) -> &mut B {
+  match try_cast_mut(a) {
+    Ok(t) => t,
+    Err(e) => something_went_wrong("cast_mut", e),
+  }
+}
+
+/// Cast `&T` into `&U`.
+///
+/// ## Panics
+///
+/// This is [`try_cast_ref`] but will panic on error.
+#[inline]
+pub fn cast_ref<A: NoPadding, B: CheckedBitPattern>(a: &A) -> &B {
+  match try_cast_ref(a) {
+    Ok(t) => t,
+    Err(e) => something_went_wrong("cast_ref", e),
+  }
+}
+
+/// Cast `&[A]` into `&[B]`.
+///
+/// ## Panics
+///
+/// This is [`try_cast_slice`] but will panic on error.
+#[inline]
+pub fn cast_slice<A: NoPadding, B: CheckedBitPattern>(a: &[A]) -> &[B] {
+  match try_cast_slice(a) {
+    Ok(t) => t,
+    Err(e) => something_went_wrong("cast_slice", e),
+  }
+}
+
+/// Cast `&mut [T]` into `&mut [U]`.
+///
+/// ## Panics
+///
+/// This is [`try_cast_slice_mut`] but will panic on error.
+#[inline]
+pub fn cast_slice_mut<A: NoPadding + AnyBitPattern, B: NoPadding + CheckedBitPattern>(a: &mut [A]) -> &mut [B] {
+  match try_cast_slice_mut(a) {
+    Ok(t) => t,
+    Err(e) => something_went_wrong("cast_slice_mut", e),
+  }
+}

Cargo.toml
@@ -29,8 +29,8 @@ nightly_portable_simd = []
 
 [dependencies]
 # use the upper line for testing against bytemuck_derive changes, if any
-#bytemuck_derive = { version = "1.0.1-alpha.0", path = "derive", optional = true }
-bytemuck_derive = { version = "1", optional = true }
+bytemuck_derive = { path = "./derive", optional = true }
+# bytemuck_derive = { version = "1", optional = true }
 
 [package.metadata.docs.rs]
 # Note(Lokathor): Don't use all-feautures or it would use `unsound_ptr_pod_impl` too.

derive/Cargo.toml
@@ -20,4 +20,4 @@ quote = "1"
 proc-macro2 = "1"
 
 [dev-dependencies]
-bytemuck = "1.2"
+bytemuck = { path = "../", features = ["derive"] }

derive/src/lib.rs
@@ -8,7 +8,9 @@ use proc_macro2::TokenStream;
 use quote::quote;
 use syn::{parse_macro_input, DeriveInput};
 
-use crate::traits::{Contiguous, Derivable, Pod, TransparentWrapper, Zeroable};
+use crate::traits::{
+  AnyBitPattern, Contiguous, Derivable, CheckedBitPattern, NoPadding, Pod, TransparentWrapper, Zeroable,
+};
 
 /// Derive the `Pod` trait for a struct
 ///
@@ -42,6 +44,24 @@ pub fn derive_pod(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
   proc_macro::TokenStream::from(expanded)
 }
 
+/// Derive the `AnyBitPattern` trait for a struct
+///
+/// The macro ensures that the struct follows all the the safety requirements
+/// for the `AnyBitPattern` trait.
+///
+/// The following constraints need to be satisfied for the macro to succeed
+///
+/// - All fields ind the struct must to implement `AnyBitPattern`
+#[proc_macro_derive(AnyBitPattern)]
+pub fn derive_anybitpattern(
+  input: proc_macro::TokenStream,
+) -> proc_macro::TokenStream {
+  let expanded =
+    derive_marker_trait::<AnyBitPattern>(parse_macro_input!(input as DeriveInput));
+
+  proc_macro::TokenStream::from(expanded)
+}
+
 /// Derive the `Zeroable` trait for a struct
 ///
 /// The macro ensures that the struct follows all the the safety requirements
@@ -69,6 +89,61 @@ pub fn derive_zeroable(
 ) -> proc_macro::TokenStream {
   let expanded =
     derive_marker_trait::<Zeroable>(parse_macro_input!(input as DeriveInput));
+
+  proc_macro::TokenStream::from(expanded)
+}
+
+/// Derive the `NoPadding` trait for a struct or enum
+///
+/// The macro ensures that the type follows all the the safety requirements
+/// for the `NoPadding` trait.
+///
+/// The following constraints need to be satisfied for the macro to succeed
+/// (the rest of the constraints are guaranteed by the `NoPadding` subtrait bounds,
+/// i.e. the type must be `Sized + Copy + 'static`):
+///
+/// If applied to a struct:
+/// - All fields in the struct must implement `NoPadding`
+/// - The struct must be `#[repr(C)]` or `#[repr(transparent)]`
+/// - The struct must not contain any padding bytes
+/// - The struct must contain no generic parameters
+///
+/// If applied to an enum:
+/// - The enum must be explicit `#[repr(Int)]`
+/// - All variants must be fieldless
+/// - The enum must contain no generic parameters
+#[proc_macro_derive(NoPadding)]
+pub fn derive_no_padding(
+  input: proc_macro::TokenStream,
+) -> proc_macro::TokenStream {
+  let expanded =
+    derive_marker_trait::<NoPadding>(parse_macro_input!(input as DeriveInput));
+
+  proc_macro::TokenStream::from(expanded)
+}
+
+/// Derive the `CheckedBitPattern` trait for a struct or enum.
+///
+/// The macro ensures that the type follows all the the safety requirements
+/// for the `CheckedBitPattern` trait and derives the required `Bits` type
+/// definition and `is_valid_bit_pattern` method for the type automatically.
+///
+/// The following constraints need to be satisfied for the macro to succeed
+/// (the rest of the constraints are guaranteed by the `CheckedBitPattern` subtrait bounds,
+/// i.e. are guaranteed by the requirements of the `NoPadding` trait which `CheckedBitPattern`
+/// is a subtrait of):
+///
+/// If applied to a struct:
+/// - All fields must implement `CheckedBitPattern`
+///
+/// If applied to an enum:
+/// - All requirements already checked by `NoPadding`, just impls the trait
+#[proc_macro_derive(CheckedBitPattern)]
+pub fn derive_maybe_pod(
+  input: proc_macro::TokenStream,
+) -> proc_macro::TokenStream {
+  let expanded =
+    derive_marker_trait::<CheckedBitPattern>(parse_macro_input!(input as DeriveInput));
 
   proc_macro::TokenStream::from(expanded)
 }
@@ -164,16 +239,26 @@ fn derive_marker_trait_inner<Trait: Derivable>(
     input.generics.split_for_impl();
 
   let trait_ = Trait::ident();
-  Trait::check_attributes(&input.attrs)?;
-  let asserts = Trait::struct_asserts(&input)?;
+  Trait::check_attributes(&input.data, &input.attrs)?;
+  let asserts = Trait::asserts(&input)?;
   let trait_params = Trait::generic_params(&input)?;
-  let trait_impl = Trait::trait_impl(&input)?;
+  let (trait_impl_extras, trait_impl) = Trait::trait_impl(&input)?;
+
+  let implies_trait = if let Some(implies_trait) = Trait::implies_trait() {
+    quote!(unsafe impl #implies_trait for #name {})
+  } else {
+    quote!()
+  };
 
   Ok(quote! {
     #asserts
 
+    #trait_impl_extras
+
     unsafe impl #impl_generics #trait_ #trait_params for #name #ty_generics #where_clause {
       #trait_impl
     }
+
+    #implies_trait
   })
 }

commit 6a094625a70da4e4ce4f13aa8024532509492126

Make `checked::pod_read_unaligned` require `T: CheckedBitPattern`
Currently, `checked::pod_read_unaligned` requires `T: AnyBitPattern`. (This was likely just a copy-paste error from when the function was introduced in #91.)
There should be no current UB nor semver breakage, since this only relaxes the bound (any type this previously worked with was already sound and will continue to work).
`checked::try_pod_read_unaligned has the correct `T: CheckedBitPattern` bound.

src/checked.rs
@@ -404,7 +404,7 @@ pub fn from_bytes_mut<T: NoUninit + CheckedBitPattern>(s: &mut [u8]) -> &mut T {
 /// ## Panics
 /// * This is like `try_pod_read_unaligned` but will panic on failure.
 #[inline]
-pub fn pod_read_unaligned<T: AnyBitPattern>(bytes: &[u8]) -> T {
+pub fn pod_read_unaligned<T: CheckedBitPattern>(bytes: &[u8]) -> T {
   match try_pod_read_unaligned(bytes) {
     Ok(t) => t,
     Err(e) => something_went_wrong("pod_read_unaligned", e),