Lokathor/bytemuck

commit 5f9ce738730c0c516b2d3d2f6025897d43860808

replace #[derive(Debug)] with manual impls
The new impls should be identical, but they have the advantage that we
can move them around. See the next patch for why this is useful.

derive/src/traits.rs
@@ -607,18 +607,23 @@ fn generate_checked_bit_pattern_struct(
   let field_name = &field_names[..];
   let field_ty = &field_tys[..];
 
-  let derive_dbg =
-    quote!(#[cfg_attr(not(target_arch = "spirv"), derive(Debug))]);
-
   Ok((
     quote! {
         #[doc = #GENERATED_TYPE_DOCUMENTATION]
         #repr
         #[derive(Clone, Copy, #crate_name::AnyBitPattern)]
-        #derive_dbg
         #[allow(missing_docs)]
         pub struct #bits_ty {
             #(#field_name: <#field_ty as #crate_name::CheckedBitPattern>::Bits,)*
+        }
+
+        #[cfg(not(target_arch = "spirv"))]
+        impl ::core::fmt::Debug for #bits_ty {
+          fn fmt(&self, f: &mut ::core::fmt::Formatter<'_>) -> ::core::fmt::Result {
+            let mut debug_struct = ::core::fmt::Formatter::debug_struct(f, ::core::stringify!(#bits_ty));
+            #(::core::fmt::DebugStruct::field(&mut debug_struct, ::core::stringify!(#field_name), &self.#field_name);)*
+            ::core::fmt::DebugStruct::finish(&mut debug_struct)
+          }
         }
     },
     quote! {
@@ -711,9 +716,6 @@ fn generate_checked_bit_pattern_enum_with_fields(
   let representation = get_repr(&input.attrs)?;
   let vis = &input.vis;
 
-  let derive_dbg =
-    quote!(#[cfg_attr(not(target_arch = "spirv"), derive(Debug))]);
-
   match representation.repr {
     Repr::Rust => unreachable!(),
     repr @ (Repr::C | Repr::CWithDiscriminant(_)) => {
@@ -793,11 +795,20 @@ fn generate_checked_bit_pattern_enum_with_fields(
         quote! {
           #[doc = #GENERATED_TYPE_DOCUMENTATION]
           #[derive(::core::clone::Clone, ::core::marker::Copy, #crate_name::AnyBitPattern)]
-          #derive_dbg
           #bits_repr
           #vis struct #bits_ty_ident {
             tag: #integer,
             payload: #variants_union_ident,
+          }
+
+          #[cfg(not(target_arch = "spirv"))]
+          impl ::core::fmt::Debug for #bits_ty_ident {
+            fn fmt(&self, f: &mut ::core::fmt::Formatter<'_>) -> ::core::fmt::Result {
+              let mut debug_struct = ::core::fmt::Formatter::debug_struct(f, ::core::stringify!(#bits_ty_ident));
+              ::core::fmt::DebugStruct::field(&mut debug_struct, "tag", &self.tag)?;
+              ::core::fmt::DebugStruct::field(&mut debug_struct, "payload", &self.payload);
+              ::core::fmt::DebugStruct::finish(&mut debug_struct)
+            }
           }
 
           #[derive(::core::clone::Clone, ::core::marker::Copy, #crate_name::AnyBitPattern)]

commit 4d4106bbcca5e4d1f54428aa5150c54a6e8a5b0c

fix deriving CheckedBitPattern on packed struct
When a field in a packed struct is not sufficiently aligned, it's
illegal to create a reference to it. Consequently, our Debug
implementation should not create references.

This does not affect enums because enums can't be packed.

Fixes: 5f9ce738730c0c516b2d3d2f6025897d43860808

derive/src/traits.rs
@@ -623,7 +623,7 @@ fn generate_checked_bit_pattern_struct(
           impl ::core::fmt::Debug for #bits_ty {
             fn fmt(&self, f: &mut ::core::fmt::Formatter<'_>) -> ::core::fmt::Result {
               let mut debug_struct = ::core::fmt::Formatter::debug_struct(f, ::core::stringify!(#bits_ty));
-              #(::core::fmt::DebugStruct::field(&mut debug_struct, ::core::stringify!(#field_name), &self.#field_name);)*
+              #(::core::fmt::DebugStruct::field(&mut debug_struct, ::core::stringify!(#field_name), &{ self.#field_name });)*
               ::core::fmt::DebugStruct::finish(&mut debug_struct)
             }
           }