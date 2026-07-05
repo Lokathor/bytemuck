5b102fa0cdf4476924ba10c5f925a4e2307fe165
Josh Megnauth <jo.sh@tutanota.com>
2026-07-04 21:23:23 -0400

    Don't impl core::error::Error on spirv
    Fixes a build error on spirv. Display is implemented for error types on
    non-spirv targets, so implementing core::error::Error needs to match
    that.

--- src/lib.rs
+++ src/lib.rs
@@ -269,7 +269,11 @@ impl core::fmt::Display for PodCastError {
 impl std::error::Error for PodCastError {}
 
 // Rust 1.81+
-#[cfg(all(feature = "impl_core_error", not(feature = "extern_crate_std")))]
+#[cfg(all(
+  feature = "impl_core_error",
+  not(feature = "extern_crate_std"),
+  not(target_arch = "spirv")
+))]
 impl core::error::Error for PodCastError {}
 
 /// Re-interprets `&T` as `&[u8]`.
